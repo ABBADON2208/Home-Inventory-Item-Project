6de85201325ace3616a5078dfb134ea1c9ea15e3
Ankit Kumar <68867530+ABBADON2208@users.noreply.github.com>
2021-05-20 19:13:05 +0530

    Update HomeInventory.java

--- HomeInventory.java
+++ HomeInventory.java
@@ -710,17 +710,4 @@ public class HomeInventory{
 }
 
 
-   class InventoryItem {
-	public String item;
-	public String location;
-	public boolean marked;
-	public String price;
-	public String date;
-	public String store;
-	public String note;
-	public String src;
-	public String id;
-	public String toString() {
-		return item+" "+location;
-	}
-}
+
